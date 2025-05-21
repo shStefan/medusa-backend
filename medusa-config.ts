36c901c13b2bbca7504d6eae8b1a37812d4882b2
Stefan Shakhazizyan <stefanshakhazizyan@gmail.com>
2025-05-21 16:47:43 +0800

    Fix database configuration property names

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,12 +5,19 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
-    http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+    databaseType: "postgres",
+    storeCors: process.env.STORE_CORS || "http://localhost:8000",
+    adminCors: process.env.ADMIN_CORS || "http://localhost:7000",
+    authCors: process.env.AUTH_CORS || "http://localhost:7000",
+    jwtSecret: process.env.JWT_SECRET || "supersecret",
+    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+  },
+  modules: {
+    eventBus: {
+      resolve: "@medusajs/event-bus-local",
+    },
+    cacheService: {
+      resolve: "@medusajs/cache-inmemory",
+    },
+  },
 })
